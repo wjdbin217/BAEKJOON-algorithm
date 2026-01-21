86f1fecb69bf547605c6ec0bcddbf659f1ad9f37
wjd_bin217 <156114758+wjdbin217@users.noreply.github.com>
2026-01-21 22:25:49 +0900

    260121 : 2798 solved

---
+++ 단계별/12_브루트 포스/2798.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+int main() {
+
+	int N, M;
+	cin >> N >> M;
+
+	vector<int> vec;
+
+	for (int i = 0; i < N; i++)
+	{
+		int var;
+		cin >> var;
+		vec.push_back(var);
+	}
+
+	int max = 0;
+
+	// N개 중에서 순서 고려 없이 3개 뽑기
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = i + 1; j < N; j++)
+		{
+			for (int k = j + 1; k < N; k++)
+			{	
+				int sum = vec[i] + vec[j] + vec[k];
+				if (max < sum && sum <= M)
+				{
+					max = sum;
+				}
+			}
+		}
+	}
+
+	cout << max;
+
+	return 0;
+}
