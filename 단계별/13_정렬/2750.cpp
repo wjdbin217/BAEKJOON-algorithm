c5791a52f51406143823653cbd500e36e8ab711f
wjd_bin217 <156114758+wjdbin217@users.noreply.github.com>
2026-01-23 23:12:26 +0900

    260123 : 2750 solved

---
+++ 단계별/13_정렬/2750.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+#include <algorithm>   // swap 사용하기 위해서
+
+
+// quickSort 사용
+
+int partition(int arr[], int low, int high)
+{
+	int pivot = arr[high]; // 기준값
+	int i = low - 1;
+
+	for (int j = low; j < high; j++)
+	{
+		if (arr[j] < pivot)
+		{
+			i++;
+			swap(arr[i], arr[j]);
+		}
+	}
+	swap(arr[i + 1], arr[high]);
+	return i + 1;
+}
+
+void quickSort(int arr[], int low, int high)
+{
+	if (low < high)
+	{
+		int pi = partition(arr, low, high);
+		quickSort(arr, low, pi - 1);
+		quickSort(arr, pi + 1, high);
+	}
+}
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	int arr[1000];
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	quickSort(arr, 0, n - 1);
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << '\n';
+	}
+
+	return 0;
+}
