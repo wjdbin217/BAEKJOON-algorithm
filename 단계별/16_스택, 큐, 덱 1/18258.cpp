3dbf933946bc4700848c0fd8f6453c2186811504
wjd_bin217 <156114758+wjdbin217@users.noreply.github.com>
2026-01-27 21:10:53 +0900

    260127 : PS

--- 단계별/16_스택, 큐, 덱 1/18258.cpp
+++ 단계별/16_스택, 큐, 덱 1/18258.cpp
@@ -1,8 +1,73 @@
 #include <iostream>
 using namespace std;
+#include <queue>
+#include <string>
 
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
 	
+	int n;
+	cin >> n;
+	queue<int> que;
+
+	for (int i = 0; i < n; i++)
+	{
+		string str;
+		cin >> str;
+		
+		if (str == "push")
+		{
+			int x;
+			cin >> x;
+			que.push(x);
+		}
+		else if (str == "pop")
+		{
+			if (que.empty())
+			{
+				cout << "-1\n";
+			}
+			else
+			{
+				cout << que.front() << '\n';
+				que.pop();
+			}
+		}
+		else if (str == "size")
+		{
+			cout << que.size() << '\n';
+		}
+		else if (str == "empty")
+		{
+			if (que.empty())
+			{
+				cout << "1\n";
+			}
+			else 
+				cout << "0\n";
+		}
+		else if (str == "front")
+		{
+			if (que.empty())
+			{
+				cout << "-1\n";
+			}
+			else 
+			{
+				cout << que.front() << '\n';
+			}
+		}
+		else if (str == "back")
+		{
+			if (que.empty())
+			{
+				cout << "-1\n";
+			}
+			else
+				cout << que.back() << '\n';
+		}
+	}
 
 
 
